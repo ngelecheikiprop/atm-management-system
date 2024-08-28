17f7b956e88c9ede9c89d9e060e01a4f04a84440
ngelecheikiprop <ngelecheikiprop@gmail.com>
2024-08-28 12:20:29 +0300

    added printing the type of data

--- api/v1/views/callback.py
+++ api/v1/views/callback.py
@@ -1,5 +1,7 @@
 from api.v1.views import app_views
 from flask import request
+from models import storage
+from models.payments import Payment
 
 @app_views.route("/callback", methods=['POST'], strict_slashes=False)
 def callback():
@@ -13,5 +15,5 @@ def callback():
     """
     print("recieving data")
     data = request.get_json()
-    print(data)
+    print(type(data))
     return "ok"
