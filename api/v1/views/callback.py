eac10bb022f233472b20ba566ac356a33d416d96
ngelecheikiprop <ngelecheikiprop@gmail.com>
2024-08-28 12:29:15 +0300

    storing the data

--- api/v1/views/callback.py
+++ api/v1/views/callback.py
@@ -15,5 +15,17 @@ def callback():
     """
     print("recieving data")
     data = request.get_json()
-    print(type(data))
+    # Extract relevant data
+    callback_metadata = data['Body']['stkCallback']['CallbackMetadata']['Item']
+    payment_info = {item['Name']: item['Value'] for item in callback_metadata}
+    # Create Payment object
+    payment = Payment(
+            phone_number=str(payment_info.get("PhoneNumber")),
+            transaction_id=payment_info.get("MpesaReceiptNumber"),
+            initiation_id=data['Body']['stkCallback']['CheckoutRequestID'],
+            amount=int(payment_info.get("Amount")),
+            time_received=datetime.fromtimestamp(payment_info.get("TransactionDate") / 1000.0)
+            )
+    storage.new(payment)
+    storage.save()
     return "ok"
