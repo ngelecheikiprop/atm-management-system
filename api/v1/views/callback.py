da3fc9146fde5862313e2174712842482c7384c2
ngelecheikiprop <ngelecheikiprop@gmail.com>
2024-08-28 11:38:06 +0000

    finally I can store the transaction

--- api/v1/views/callback.py
+++ api/v1/views/callback.py
@@ -17,11 +17,11 @@ def callback():
     data = request.get_json()
     # Extract relevant data
     callback_metadata = data['Body']['stkCallback']['CallbackMetadata']['Item']
-    payment_info = {item['Name']: item['Value'] for item in callback_metadata}
+    payment_info = {item.get('Name'): item.get('Value') for item in callback_metadata}
     # Create Payment object
     time_received_str = str(payment_info.get("TransactionDate"))
     time_format = "%Y%m%d%H%M%S"
-    time_recieved = datetime.strptime(time_recieved_str, time_format)
+    time_recieved = datetime.strptime(time_received_str, time_format)
     payment = Payment(
             phone_number=str(payment_info.get("PhoneNumber")),
             transaction_id=payment_info.get("MpesaReceiptNumber"),
