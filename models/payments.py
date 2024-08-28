07dd1371239e84413ae00699ea4d3fece32ec9fd
ngelecheikiprop <ngelecheikiprop@gmail.com>
2024-08-28 08:52:52 +0300

    latest on payments

--- models/payments.py
+++ models/payments.py
@@ -1,5 +1,10 @@
 """Stores the transactions completed
 """
+from sqlalchemy import Column, String, Integer, DateTime, create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from datetime import datetime
+
+from models import Base
 class Payment(Base):
     """Defines the table to store transactions completed
     """
