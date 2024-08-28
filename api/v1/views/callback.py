453e4a00abb3ed089011b7173d64aa1b774a7cf7
ngelecheikiprop <ngelecheikiprop@gmail.com>
2024-08-28 07:43:11 +0300

    the url for handling callback

---
+++ api/v1/views/callback.py
@@ -0,0 +1,16 @@
+from api.v1.views import app_views
+from flask import request
+
+@app_views.route("/callback", methods=['POST'], strict_slashes=False)
+def callback():
+    """get data and store
+    """
+    """
+    get the data in varibales
+    make payment object
+    new 
+    save
+    """
+    print("recieving data")
+    data = request.get_json()
+    print(data)
