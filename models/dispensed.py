0ddf3c5d40fda48136d02657cf3fb24d1c8340d7
ngelecheikiprop <ngelecheikiprop@gmail.com>
2024-08-28 18:28:35 +0300

    added the defination of dispensed table

---
+++ models/dispensed.py
@@ -0,0 +1,13 @@
+"""The functions that will recieve data from pi and store to database
+"""
+from sqlalchemy import Column, String, Float, Integer, DateTime, ForeignKey
+from models import Base
+from datetime import datetime
+
+class Dispensed(Base):
+    """Defines the dispensed table."""
+    __tablename__ = "dispensed"
+
+    initiation_id = Column(String(50), ForeignKey("payments.initiation_id"), nullable=False)
+    litres_dispensed = Column(Float, nullable=False)
+    status = Column(String(50), nullable=False)
