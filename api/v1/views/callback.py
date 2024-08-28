1f705f64922b50c12f03063cb5b050e9d89d3927
ngelecheikiprop <ngelecheikiprop@gmail.com>
2024-08-28 14:14:20 +0300

    callback url storing

--- api/v1/views/callback.py
+++ api/v1/views/callback.py
@@ -2,7 +2,7 @@ from api.v1.views import app_views
 from flask import request
 from models import storage
 from models.payments import Payment
-
+from datetime import datetime
 @app_views.route("/callback", methods=['POST'], strict_slashes=False)
 def callback():
     """get data and store
@@ -19,12 +19,15 @@ def callback():
     callback_metadata = data['Body']['stkCallback']['CallbackMetadata']['Item']
     payment_info = {item['Name']: item['Value'] for item in callback_metadata}
     # Create Payment object
+    time_received_str = str(payment_info.get("TransactionDate"))
+    time_format = "%Y%m%d%H%M%S"
+    time_recieved = datetime.strptime(time_recieved_str, time_format)
     payment = Payment(
             phone_number=str(payment_info.get("PhoneNumber")),
             transaction_id=payment_info.get("MpesaReceiptNumber"),
             initiation_id=data['Body']['stkCallback']['CheckoutRequestID'],
             amount=int(payment_info.get("Amount")),
-            time_received=datetime.fromtimestamp(payment_info.get("TransactionDate") / 1000.0)
+            time_received=time_recieved
             )
     storage.new(payment)
     storage.save()
