9fc889dabd8cc433883754617b580b145726828f
ngelecheikiprop <ngelecheikiprop@gmail.com>
2024-08-31 14:57:22 +0300

    stores data well"

--- models/dispensed.py
+++ models/dispensed.py
@@ -7,7 +7,6 @@ from datetime import datetime
 class Dispensed(Base):
     """Defines the dispensed table."""
     __tablename__ = "dispensed"
-
-    initiation_id = Column(String(50), ForeignKey("payments.initiation_id"), nullable=False)
+    initiation_id = Column(String(50), ForeignKey("payments.initiation_id"), nullable=False, primary_key=True)
     litres_dispensed = Column(Float, nullable=False)
     status = Column(String(50), nullable=False)
